843c69069473f66dc3e5511e1b8a565675d57477
meagerindings <mgreten@gmail.com>
2017-08-16 17:40:49 -0600

    completed lynda: displaying data with a listview

--- java/com/meagerfindings/matgreten/c196_student_scheduler_mat_greten/HomeScreen.java
+++ java/com/meagerfindings/matgreten/c196_student_scheduler_mat_greten/HomeScreen.java
@@ -1,13 +1,17 @@
 package com.meagerfindings.matgreten.c196_student_scheduler_mat_greten;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import static com.meagerfindings.matgreten.c196_student_scheduler_mat_greten.NotesProvider.*;
 
@@ -19,6 +23,20 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
 
         insertNote("New note");
+
+        Cursor cursor = getContentResolver().query(NotesProvider.CONTENT_URI, DBOpenHelper.ALL_COLUMNS, null, null, null, null);
+
+        String[] from = {DBOpenHelper.NOTE_TEXT};
+        int[] to = {android.R.id.text1};
+        CursorAdapter cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,cursor, from, to, 0);
+
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
+
+        assert cursor != null;
+        cursor.close();
+
+
 
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
@@ -38,6 +56,7 @@ public class HomeScreen extends AppCompatActivity {
         values.put(DBOpenHelper.NOTE_TEXT, noteText);
         Uri noteUri = getContentResolver().insert(CONTENT_URI, values);
 
+        assert noteUri != null;
         Log.d("HomeScreenActivity", "Inserted note " + noteUri.getLastPathSegment());
     }
 
