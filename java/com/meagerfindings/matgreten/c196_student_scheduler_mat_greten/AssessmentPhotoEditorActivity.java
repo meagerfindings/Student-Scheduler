c999ab2b61a90517e6771e98fb2a387f165c8dd3
meagerindings <mgreten@gmail.com>
2017-09-09 16:06:53 -0600

    stubbed camera integration

--- java/com/meagerfindings/matgreten/c196_student_scheduler_mat_greten/AssessmentPhotoEditorActivity.java
+++ java/com/meagerfindings/matgreten/c196_student_scheduler_mat_greten/AssessmentPhotoEditorActivity.java
@@ -5,11 +5,20 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.meagerfindings.matgreten.c196_student_scheduler_mat_greten.ScheduleContract.AssessmentPhotoEntry;
 
@@ -20,6 +29,7 @@ public class AssessmentPhotoEditorActivity extends AppCompatActivity{
     private String assessmentPhotoFilter;
     private String oldFile;
     private String assessmentNoteID;
+    private Uri bmpUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +59,10 @@ public class AssessmentPhotoEditorActivity extends AppCompatActivity{
             oldFile = cursor.getString(cursor.getColumnIndex(AssessmentPhotoEntry.ASSESSMENT_PHOTO));
 //            fileEditor.setImageURI(oldFile);
             fileEditor.setImageURI(uri);
+
+            // TODO CITE: https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+            File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+            bmpUri = FileProvider.getUriForFile(this, "file_provider", file);
         }
     }
 
@@ -90,6 +104,48 @@ public class AssessmentPhotoEditorActivity extends AppCompatActivity{
 //        finish();
 //    }
 
+    // TODO CITE: https://developer.android.com/training/camera/photobasics.html#TaskCaptureIntent
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    public void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                 System.out.println("Error occurred while creating the File");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "file_provider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
     private void deleteAssessmentPhoto() {
         getContentResolver().delete(AssessmentPhotoEntry.CONTENT_URI, assessmentPhotoFilter, null);
         Toast.makeText(this, R.string.photo_deleted, Toast.LENGTH_SHORT).show();
@@ -125,5 +181,9 @@ public class AssessmentPhotoEditorActivity extends AppCompatActivity{
             getMenuInflater().inflate(R.menu.menu_editor, menu);
         }
         return true;
+    }
+
+    public void takePicture(View view) {
+        dispatchTakePictureIntent();
     }
 }
