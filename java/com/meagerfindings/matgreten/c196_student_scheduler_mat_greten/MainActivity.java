1752530d9590f7ba3386039e3342bb9db15d0a7d
meagerindings <mgreten@gmail.com>
2017-09-17 15:48:53 -0600

    main screen checks for dependancies

--- java/com/meagerfindings/matgreten/c196_student_scheduler_mat_greten/MainActivity.java
+++ java/com/meagerfindings/matgreten/c196_student_scheduler_mat_greten/MainActivity.java
@@ -1,10 +1,17 @@
 package com.meagerfindings.matgreten.c196_student_scheduler_mat_greten;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import static com.meagerfindings.matgreten.c196_student_scheduler_mat_greten.ScheduleContract.*;
+import static com.meagerfindings.matgreten.c196_student_scheduler_mat_greten.ScheduleContract.TABLE_COURSES;
+import static com.meagerfindings.matgreten.c196_student_scheduler_mat_greten.ScheduleContract.TABLE_TERMS;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,12 +30,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void openCourses(View view) {
         Intent intent = new Intent(this, CoursesActivity.class);
-        startActivity(intent);
+        ScheduleDBHelper handler = new ScheduleDBHelper(this);
+        String queryString = "SELECT " + TermEntry.TERM_TITLE + " FROM " + TABLE_TERMS;
+        SQLiteDatabase db = handler.getWritableDatabase();
+        Cursor termCursor = db.rawQuery(queryString, null);
+        if (termCursor.moveToFirst()) startActivity(intent);
+        else Toast.makeText(this, R.string.no_terms, Toast.LENGTH_LONG).show();
+        termCursor.close();
+        db.close();
     }
 
     public void openAssessments(View view) {
         Intent intent = new Intent(this, AssessmentActivity.class);
-        startActivity(intent);
+        ScheduleDBHelper handler = new ScheduleDBHelper(this);
+        String queryString = "SELECT " + CourseEntry.COURSE_TITLE+ " FROM " + TABLE_COURSES;
+        SQLiteDatabase db = handler.getWritableDatabase();
+        Cursor termCursor = db.rawQuery(queryString, null);
+        if (termCursor.moveToFirst()) startActivity(intent);
+        else Toast.makeText(this, R.string.no_courses, Toast.LENGTH_LONG).show();
+        termCursor.close();
+        db.close();
     }
 
 
--- java/com/meagerfindings/matgreten/c196_student_scheduler_mat_greten/readme.md
+++ java/com/meagerfindings/matgreten/c196_student_scheduler_mat_greten/readme.md
@@ -6,7 +6,8 @@
 
 1. ~~Ability to highlight terms to delete them~~
     - Switch delete to select, and add delete to the action bar options
-3. disable adding of courses without 
+3. ~~disable adding of courses without terms on so on down the line~~
+3. disable buttons throughout when in insert mode
 2. Portrait and landscape modes
 
 ### Bugs:
